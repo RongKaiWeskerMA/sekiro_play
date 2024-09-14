f99734bd3fd59a230113b56dd46231000ec54a33
wesker <123marongkai@gmail.com>
2024-09-14 14:39:38 +0800

    modify dataset

--- dataset/bc_dataset.py
+++ dataset/bc_dataset.py
@@ -4,6 +4,7 @@ import cv2
 import torch
 import pandas as pd
 from collections import OrderedDict
+import os
 
 
 
@@ -21,6 +22,7 @@ class SekiroDataset(Dataset):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         
+        self.label_path = [os.path.join(data_dir, f'session_{i}', "label.csv") for i in range(1, 11)]
         self.key2action =  {
             'w': 0,
             'a': 1,
@@ -43,18 +45,20 @@ class SekiroDataset(Dataset):
         state_tensor = state_tensor.unsqueeze(0)
         return state_tensor 
     
-    def read_label(self, label_path):
-        df = pd.read_csv(label_path, header=None)
-        keys = df.iloc[0, 2:] 
-        mapped_values = OrderedDict()
-        for index, row in df.iterrows():
-            if index == 0:
-                continue  # Skip the first row
-            img_name = row[0]
-            for i in range(len(keys)):
-                if float(row[i+2]) == 1:
-                    mapped_values[img_name] = self.key2action[keys[i + 2]]
-                    break
-                else:
-                    mapped_values[img_name] = self.key2action["other"]
-        return mapped_values
+    def read_label(self, label_paths):
+        for label_path in label_paths:
+            df = pd.read_csv(label_path, header=None)
+            keys = df.iloc[0, 2:] 
+            for index, row in df.iterrows():
+                if index == 0:
+                    continue  # Skip the first row
+                img_name = row[0]
+                for i in range(len(keys)):
+                    if float(row[i+2]) == 1:
+                        action = self.key2action[keys[i + 2]]
+                        self.data.append((os.path.join(self.data_dir, 'images', img_name), action))
+                        break
+                    else:
+                        action = self.key2action["other"]
+                        self.data.append((os.path.join(self.data_dir, 'images', img_name), action))
+       
